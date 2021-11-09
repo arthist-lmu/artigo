fccabf0e245c2894867763d2ae4db071dac6ff1d
Kristbaum <max@kristenonline.de>
2021-11-10 00:45:48 +0100

    add auth check for a test user function

--- api/src/artigo_api/core/urls.py
+++ api/src/artigo_api/core/urls.py
@@ -19,13 +19,16 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.authtoken import views
 
 urlpatterns = [
     path('', include('frontend.urls')), 
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
-    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+#    path('accounts/', include('allauth.urls')),
+#    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('api-token-auth/', views.obtain_auth_token),
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
