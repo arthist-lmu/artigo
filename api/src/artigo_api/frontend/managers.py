fa0370407e5e15da76a4d731b9f07af529cee17c
Kristbaum <max@kristenonline.de>
2021-11-24 04:23:51 +0100

    rewrite account creation with adaptive model

--- api/src/artigo_api/frontend/managers.py
+++ api/src/artigo_api/frontend/managers.py
@@ -5,24 +5,40 @@ from django.db.models import Count
 from django.contrib.auth.models import BaseUserManager
 
 
-class UserManager(BaseUserManager):
-    def create_user(self, email, password=None):
+class CustomUserManager(BaseUserManager):
+    #def create_user(self, email, password=None):
+    def _create_user(self, email, username, password, **extra_fields):
         if not email:
             raise ValueError('User must have an email address.')
+        if not username:
+            raise ValueError('User must have an username.')
 
-        user = self.model(email=self.normalize_email(email))
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+
+        user = self.model(
+            email=self.normalize_email(email), 
+            username=username,
+            **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
-
         return user
+    
+    def create_user(self, email, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, username, password, **extra_fields)
 
-    def create_superuser(self, email, password):
-        user = self.create_user(email, password=password)
-        user.is_superuser = True
-        user.is_staff = True
-        user.save(using=self._db)
 
-        return user
+    def create_superuser(self, email, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email=email, username=username, password=password, **extra_fields)
 
 
 class ResourceManager(models.Manager):
