48fc5668f63b0ac0b4437e2cb8897d0cdb840d3a
Kristbaum <max@kristenonline.de>
2021-11-27 02:34:39 +0100

    add some initial theming :)

--- web/src/plugins/vuetify.js
+++ web/src/plugins/vuetify.js
@@ -4,4 +4,15 @@ import 'vuetify/dist/vuetify.min.css';
 import '@mdi/font/css/materialdesignicons.css';
 
 Vue.use(Vuetify);
-export default new Vuetify({});
+export default new Vuetify({
+    theme: {
+        themes: {
+          light: {
+            primary: '#b7c8e0',
+            secondary: '#cdcdcd',
+            accent: '#2056ae',
+            error: '#ab91a1',
+          },
+        },
+      },
+});
