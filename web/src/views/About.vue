f11beb1ae6ad2892bf4c944ae181eec6bc3b74d4
Kristbaum <max@kristenonline.de>
2021-12-06 02:48:57 +0100

    about page multilingual

---
+++ web/src/views/About.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app id="about">
+    <h1>{{ $t("about.goals") }}</h1>
+        <p>{{ $t("about.p_1") }}</p>
+        <p>{{ $t("about.p_2") }}</p>
+    <h2>{{ $t("about.sources") }}</h2>
+        <p>{{ $t("about.p_3") }}</p>
+    <h2>{{ $t("about.project_funding") }}</h2>
+        <p>{{ $t("about.p_4") }}</p>
+    <h2>{{ $t("about.people") }}</h2>
+        <p>{{ $t("about.p_5") }}</p>
+        <p>Import Members list</p>
+    <h2>{{ $t("about.version_history") }}</h2>
+        <p>{{ $t("about.p_6") }}</p>
+  </v-app>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
