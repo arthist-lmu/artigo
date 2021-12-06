cf288a11846b9ff71eb27ae36e9ad69667cdedcc
Kristbaum <max@kristenonline.de>
2021-12-06 02:49:11 +0100

    language switcher

--- web/src/components/AppBar.vue
+++ web/src/components/AppBar.vue
@@ -4,17 +4,61 @@
     flat
   >
   <v-spacer></v-spacer>
+    <v-menu
+      top
+      :offset-y="offset"
+      :close-on-click="closeOnClick"
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          color="primary"
+          dark
+          v-bind="attrs"
+          v-on="on"
+        >
+          {{locale}}
+        </v-btn>
+      </template>
+
+      <v-list>
+        <v-list-item
+          v-for="(lang, index) in lang_list"
+          :key="index"
+          @click="onButtonClick(lang)"
+        >
+          <v-list-item-title>{{ lang }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
     <UserMenu />
   </v-app-bar>
 </template>
 
 <script>
 import UserMenu from '@/components/UserMenu.vue';
+import i18n from '@/plugins/i18n';
 
 export default {
-  data: () => ({ drawer: null }),
+  data: () => ({ 
+    items: [
+      { title: 'Click Me' },
+      { title: 'Click Me' },
+      { title: 'Click Me' },
+      { title: 'Click Me 2' },
+    ],
+    closeOnClick: true,
+    offset: true,
+    locale: i18n.locale,
+    lang_list: ["en", "de"],
+    reactive: true,
+  }),
   components: {
     UserMenu,
   },
+  methods: {
+    onButtonClick(lang) {
+      i18n.locale = lang
+    }
+  }
 };
 </script>
