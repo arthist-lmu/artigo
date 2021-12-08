084c087a9d210a4dfa86f9140c4fa6763a78447b
Kristbaum <max@kristenonline.de>
2021-12-08 16:12:40 +0100

    test progressbar

--- web/src/views/ArtigoGame.vue
+++ web/src/views/ArtigoGame.vue
@@ -1,5 +1,29 @@
 <template>
-  <div>
-    <h1>Insert ArtigoGame here!</h1>
-  </div>
+    <v-progress-linear
+        value="data.timerCount"
+    ></v-progress-linear>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      timerCount: 30,
+    };
+  },
+
+  watch: {
+    timerCount: {
+      handler(value) {
+        if (value > 0) {
+          setTimeout(() => {
+            this.timerCount -= 1;
+            console.log(this.timerCount);
+          }, 1000);
+        }
+      },
+      immediate: true, // This ensures the watcher is triggered upon creation
+    },
+  },
+};
+</script>
