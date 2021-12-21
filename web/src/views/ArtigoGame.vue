5b08b702466745dc52bb831263410fcfcebb6344
Kristbaum <max@kristenonline.de>
2021-12-21 23:14:12 +0100

    load real images and add button functionality

--- web/src/views/ArtigoGame.vue
+++ web/src/views/ArtigoGame.vue
@@ -2,12 +2,7 @@
   <div id="artigo-game">
     <v-progress-linear :value="progress"></v-progress-linear>
     <h2>Only {{ secondsLeft }} seconds left!</h2>
-    <v-img
-      id="main-image"
-      contain
-      max-height="65vh"
-      src="http://localhost:8000/media/36/21/36216392e7f137c39c006c698f8f2546.jpg"
-    ></v-img>
+    <v-img id="main-image" contain max-height="65vh" :src="data.path"></v-img>
     <v-container>
       <v-row>
         <v-text-field
@@ -15,8 +10,10 @@
           label="Discribe what you see"
           clearable
         ></v-text-field>
-        <v-btn color="primary" depressed rounded> Enter </v-btn>
-        <v-btn @click="get_res" color="accent" depressed rounded> Skip </v-btn>
+        <v-btn @click="displayImgUrl" color="primary" depressed rounded>
+          Enter
+        </v-btn>
+        <v-btn @click="getRes" color="accent" depressed rounded> Skip </v-btn>
       </v-row>
     </v-container>
   </div>
@@ -33,16 +30,71 @@ export default {
   data() {
     return {
       tagging: "",
-      secondsLeft: 30,
-      timeLimit: 30,
+      secondsLeft: 5,
+      timeLimit: 5,
       progress: 0,
     };
   },
 
   methods: {
-    get_res() {
+    getRes() {
       this.$store.dispatch("resource/get", { random: true });
-      this.dialog = false;
+    },
+    displayImgUrl() {
+      console.log(this.data.path);
+    },
+  },
+
+  computed: {
+    data() {
+      return this.$store.state.resource.data;
+    },
+    metadata() {
+      const metadata = {};
+      const fields = [
+        "titles",
+        "creators",
+        "location",
+        "institution",
+        "source",
+      ];
+      Object.keys(this.data).forEach((key) => {
+        if (fields.includes(key)) {
+          let values = this.data[key];
+          if (values && typeof values !== "boolean") {
+            if (typeof values === "string") {
+              values = [{ name: values }];
+            } else if (!this.isArray(values)) {
+              values = [values];
+            }
+            if (this.isArray(values)) {
+              metadata[key] = values;
+            }
+          }
+        }
+      });
+      return metadata;
+    },
+    title() {
+      if (this.keyInObj("titles", this.metadata)) {
+        return this.metadata.titles[0];
+      }
+      return { id: -1, name: this.$t("resource.default.title") };
+    },
+    creator() {
+      if (this.keyInObj("creators", this.metadata)) {
+        return this.metadata.creators[0];
+      }
+      return { id: -1, name: this.$t("resource.default.creator") };
+    },
+    tags() {
+      if (this.keyInObj("tags", this.data)) {
+        if (this.moreTags && this.data.tags.length > 20) {
+          return this.data.tags.slice(0, 20);
+        }
+        return this.data.tags;
+      }
+      return [];
     },
   },
 
@@ -54,13 +106,20 @@ export default {
             this.secondsLeft -= 1;
             this.progress = 100 - (100 / this.timeLimit) * this.secondsLeft;
             if (this.secondsLeft === 0) {
-              this.get_res();
+              this.getRes();
             }
           }, 1000);
         }
       },
       immediate: true, // This ensures the watcher is triggered upon creation
     },
+    "$route.params.id": function (id) {
+      this.getData(id);
+    },
+  },
+
+  created() {
+    this.getRes();
   },
 };
 </script>
