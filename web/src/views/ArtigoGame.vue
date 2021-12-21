511ce01498d461dcb625bc4b2dbafa1660ddf986
Kristbaum <max@kristenonline.de>
2021-12-21 20:08:02 +0100

    Img position adjustment and working timer

--- web/src/views/ArtigoGame.vue
+++ web/src/views/ArtigoGame.vue
@@ -1,34 +1,40 @@
 <template>
-  <div>
+  <div id="artigo-game">
     <v-progress-linear :value="progress"></v-progress-linear>
     <h2>Only {{ secondsLeft }} seconds left!</h2>
     <v-img
+      id="main-image"
+      contain
+      max-height="65vh"
       src="http://localhost:8000/media/36/21/36216392e7f137c39c006c698f8f2546.jpg"
     ></v-img>
     <v-container>
       <v-row>
         <v-text-field
           v-model="tagging"
-          solo
           label="Discribe what you see"
           clearable
         ></v-text-field>
-        <v-btn color="primary" heigth="50"> Primary </v-btn>
-        <v-btn @click="get_res" color="accent" depressed rounded block>
-          {{ data }}
-        </v-btn>
+        <v-btn color="primary" depressed rounded> Enter </v-btn>
+        <v-btn @click="get_res" color="accent" depressed rounded> Skip </v-btn>
       </v-row>
     </v-container>
   </div>
 </template>
+
+<style>
+#main-image {
+  margin: 5%;
+}
+</style>
 
 <script>
 export default {
   data() {
     return {
       tagging: "",
-      secondsLeft: 40,
-      timeLimit: 40,
+      secondsLeft: 30,
+      timeLimit: 30,
       progress: 0,
     };
   },
@@ -47,6 +53,9 @@ export default {
           setTimeout(() => {
             this.secondsLeft -= 1;
             this.progress = 100 - (100 / this.timeLimit) * this.secondsLeft;
+            if (this.secondsLeft === 0) {
+              this.get_res();
+            }
           }, 1000);
         }
       },
