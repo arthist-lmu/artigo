7c18ca39640255165cebec92f97967cb398a8f52
stefanieschneider <stefanie.schneider@cereality.net>
2022-09-28 18:23:17 +0200

    Move HomeView computations to regularly updated cache

--- api/src/artigo_api/frontend/cache.py
+++ api/src/artigo_api/frontend/cache.py
@@ -1,9 +1,14 @@
+import random
 import logging
 
 from django.core.cache import cache
 from django.db.models import Count
 from django.db.models.functions import Coalesce
-from frontend.models import Resource
+from frontend.utils import media_url_to_image
+from frontend.models import (
+    Resource,
+    UserTagging,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -51,3 +56,147 @@ def resource_tagging_count(**kwargs):
         cache.set(kwargs['name'], values, timeout)
 
     return values
+
+
+@name
+def random_game_parameters(**kwargs):
+    def create_game(game, variant, lang):
+        game['type'] = variant['type']
+        game['field'] = variant['field']
+
+        if game['field'] == 'tags':
+            if game['type'] == 'color':
+                colors = [
+                    {'de': 'schwarz',           'en': 'black'},
+                    {'de': 'weiß',              'en': 'white'},
+                    {'de': 'grau',              'en': 'grey'},
+                    {'de': 'rot',               'en': 'red'},
+                    {'de': 'blau',              'en': 'blue'},
+                    {'de': 'braun',             'en': 'brown'},
+                    {'de': 'grün',              'en': 'green'},
+                    {'de': 'gelb',              'en': 'yellow'},
+                ]
+
+                game['query'] = random.choice(colors)
+            elif game['type'] == 'epoch':
+                epochs = [
+                    {'de': 'renaissance',       'en': 'renaissance'},
+                    {'de': 'manierismus',       'en': 'mannerism'},
+                    {'de': 'barock',            'en': 'baroque'},
+                    {'de': 'rokoko',            'en': 'rococo'},
+                    {'de': 'romantik',          'en': 'romanticism'},
+                    {'de': 'realismus',         'en': 'realism'},
+                    {'de': 'impressionismus',   'en': 'impressionism'},
+                    {'de': 'pointillismus',     'en': 'pointillism'},
+                    {'de': 'symbolismus',       'en': 'symbolism'},
+                    {'de': 'expressionismus',   'en': 'expressionism'},
+                    {'de': 'kubismus',          'en': 'cubism'},
+                ]
+
+                game['query'] = random.choice(epochs)
+            else:
+                tags = UserTagging.objects \
+                    .filter(tag__language=lang) \
+                    .values(
+                        'tag',
+                        'tag__name',
+                    ) \
+                    .annotate(Count('tag')) \
+                    .order_by('-tag__count')
+
+                tag = tags[random.randint(0, 149)]
+                game['query'] = {lang: tag['tag__name']}
+
+            resources = UserTagging.objects \
+                .exclude(resource__hash_id__exact='') \
+                .filter(tag__name__in=game['query'].values()) \
+                .values('resource') \
+                .annotate(Count('resource')) \
+                .filter(resource__count__gte=2) \
+                .values_list('resource', flat=True)
+
+            game['query'] = game['query'][lang]
+        elif game['field'] == 'meta.creators':
+            creators = Resource.objects \
+                .exclude(hash_id__exact='') \
+                .values(
+                    'creators',
+                    'creators__name',
+                ) \
+                .annotate(Count('creators')) \
+                .order_by('-creators__count')
+
+            creator = creators[random.randint(0, 49)]
+            game['query'] = creator['creators__name']
+
+            resources = Resource.objects \
+                .exclude(hash_id__exact='') \
+                .filter(creators=creator['creators']) \
+                .values_list('id', flat=True)
+
+        resources = list(resources)
+        random.shuffle(resources)
+
+        game['params']['resource_inputs'] = resources[:100]
+        game['params']['resource_type'] = 'custom_resource'
+
+        resource = Resource.objects.get(id=resources[0])
+        game['path'] = media_url_to_image(resource.hash_id)
+
+        return game
+
+    def get_variants():
+        values = [
+            {
+                'type': 'color',
+                'field': 'tags',
+            },
+            {
+                'type': 'epoch',
+                'field': 'tags',
+            },
+            {
+                'type': 'creator',
+                'field': 'meta.creators',
+            },
+        ]
+
+        random.shuffle(values)
+
+        return values
+
+    langs = kwargs.get('lang', 'de,en').split(',')
+
+    for lang in langs:
+        games = cache.get(f"{kwargs['name']}_{lang}")
+
+        if games is None or kwargs.get('renew'):
+            games = [
+                {
+                    'params': {
+                        'game_type': 'roi',
+                    },
+                },
+                {
+                    'params': {
+                        'game_type': 'tagging',
+                    },
+                },
+                {
+                    'params': {
+                        'game_type': 'tagging',
+                        'taboo_type': 'most_annotated_taboo',
+                    },
+                },
+            ]
+
+            for game, variant in zip(games, get_variants()):
+                while game.get('path') is None:
+                    try:
+                        game = create_game(game, variant, lang)
+                    except IndexError:
+                        pass
+
+            random.shuffle(games)
+
+    return games
--- api/src/artigo_api/frontend/views/home.py
+++ api/src/artigo_api/frontend/views/home.py
@@ -1,153 +1,18 @@
-import random
 import logging
 import traceback
 
-from django.db.models import Count
-from django.utils.decorators import method_decorator
-from django.views.decorators.cache import cache_page
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.exceptions import APIException
 from drf_spectacular.utils import extend_schema
-from frontend.utils import media_url_to_image
-from frontend.models import (
-    Resource,
-    UserTagging,
-)
+from frontend import cache
 
 logger = logging.getLogger(__name__)
-
-colors = [
-    {'de': 'schwarz',           'en': 'black'},
-    {'de': 'weiß',              'en': 'white'},
-    {'de': 'grau',              'en': 'grey'},
-    {'de': 'rot',               'en': 'red'},
-    {'de': 'blau',              'en': 'blue'},
-    {'de': 'braun',             'en': 'brown'},
-    {'de': 'grün',              'en': 'green'},
-    {'de': 'gelb',              'en': 'yellow'},
-]
-
-epochs = [
-    {'de': 'renaissance',       'en': 'renaissance'},
-    {'de': 'manierismus',       'en': 'mannerism'},
-    {'de': 'barock',            'en': 'baroque'},
-    {'de': 'rokoko',            'en': 'rococo'},
-    {'de': 'romantik',          'en': 'romanticism'},
-    {'de': 'realismus',         'en': 'realism'},
-    {'de': 'impressionismus',   'en': 'impressionism'},
-    {'de': 'pointillismus',     'en': 'pointillism'},
-    {'de': 'symbolismus',       'en': 'symbolism'},
-    {'de': 'expressionismus',   'en': 'expressionism'},
-    {'de': 'kubismus',          'en': 'cubism'},
-]
 
 
 @extend_schema(methods=['GET'], exclude=True)
 class HomeView(APIView):
-    @property
-    def variants(self):
-        values = [
-            # { 'type': 'tag', 'field': 'tags' },
-            { 'type': 'color', 'field': 'tags' },
-            { 'type': 'epoch', 'field': 'tags' },
-            { 'type': 'creator', 'field': 'meta.creators' },
-        ]
-
-        random.shuffle(values)
-
-        return values
-
-    def create(self, game, variant, lang='de'):
-        game['type'] = variant['type']
-        game['field'] = variant['field']
-
-        if game['field'] == 'tags':
-            if game['type'] == 'color':
-                game['query'] = random.choice(colors)
-            elif game['type'] == 'epoch':
-                game['query'] = random.choice(epochs)
-            else:
-                tags = UserTagging.objects \
-                    .filter(tag__language=lang) \
-                    .values(
-                        'tag',
-                        'tag__name',
-                    ) \
-                    .annotate(Count('tag')) \
-                    .order_by('-tag__count')
-
-                tag = tags[random.randint(0, 149)]
-                game['query'] = {lang: tag['tag__name']}
-
-            resources = UserTagging.objects \
-                .exclude(resource__hash_id__exact='') \
-                .filter(tag__name__in=game['query'].values()) \
-                .values('resource') \
-                .annotate(Count('resource')) \
-                .filter(resource__count__gte=2) \
-                .values_list('resource', flat=True)
-
-            game['query'] = game['query'][lang]
-        elif game['field'] == 'meta.creators':
-            creators = Resource.objects \
-                .exclude(hash_id__exact='') \
-                .values(
-                    'creators',
-                    'creators__name',
-                ) \
-                .annotate(Count('creators')) \
-                .order_by('-creators__count')
-
-            creator = creators[random.randint(0, 49)]
-            game['query'] = creator['creators__name']
-
-            resources = Resource.objects \
-                .exclude(hash_id__exact='') \
-                .filter(creators=creator['creators']) \
-                .values_list('id', flat=True)
-
-        resources = list(resources)
-        random.shuffle(resources)
-
-        game['params']['resource_inputs'] = resources[:100]
-        game['params']['resource_type'] = 'custom_resource'
-
-        resource = Resource.objects.get(id=resources[0])
-        game['path'] = media_url_to_image(resource.hash_id)
-
-        return game
-
-    @method_decorator(cache_page(60 * 60 * 2))
     def get(self, request, format=None):
-        games = [
-            {
-                'params': {
-                    'game_type': 'roi',
-                },
-            },
-            {
-                'params': {
-                    'game_type': 'tagging',
-                },
-            },
-            {
-                'params': {
-                    'game_type': 'tagging',
-                    'taboo_type': 'most_annotated_taboo',
-                },
-            },
-        ]
-
         lang = request.query_params.get('lang', 'de')
-
-        for game, variant in zip(games, self.variants):
-            while game.get('path') is None:
-                try:
-                    game = self.create(game, variant, lang)
-                except IndexError:
-                    pass
-
-        random.shuffle(games)
+        games = cache.random_game_parameters(lang=lang)
 
         return Response(games)
