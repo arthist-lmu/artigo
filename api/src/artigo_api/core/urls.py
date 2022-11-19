61ae881ee1d03d6ede9169ab4dbfc96e846515bd
sschneider <stefanie.schneider@cereality.net>
2022-11-19 16:33:04 +0100

    Add urls for password reset #122

--- api/src/artigo_api/core/urls.py
+++ api/src/artigo_api/core/urls.py
@@ -14,6 +14,5 @@ urlpatterns = [
     path('api-token-auth/', views.obtain_auth_token),
 ]
 
-
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- api/src/artigo_api/frontend/urls.py
+++ api/src/artigo_api/frontend/urls.py
@@ -1,12 +1,12 @@
 from .views import *
-from django.urls import path
+from django.urls import path, re_path
 from django.contrib import admin
 from django.conf.urls import include
-from django.views.generic import RedirectView
+from django.views.generic import RedirectView, TemplateView
 from drf_spectacular.views import (
-    SpectacularAPIView,
-    SpectacularRedocView,
-    SpectacularSwaggerView,
+    SpectacularAPIView as SchemaView,
+    SpectacularRedocView as RedocView,
+    SpectacularSwaggerView as SwaggerView,
 )
 
 urlpatterns = [
@@ -17,19 +17,27 @@ urlpatterns = [
     path('reconcile/add/', ReconcileAddView.as_view(), name='reconcile_add'),
     path('reconcile/remove/', ReconcileRemoveView.as_view(), name='reconcile_remove'),
     path('search/', SearchView.as_view(), name='search'),
-    path('scores/', ScoresView.as_view(), name='scores'),
     path('session/', SessionView.as_view(), name='session'),
     path('sessions/', SessionsView.as_view(), name='sessions'),
     path('statistics/', StatisticsView.as_view(), name='statistics'),
-    path('schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('schema/', SchemaView.as_view(), name='schema'),
+    path('schema/swagger-ui/', SwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('schema/redoc/', RedocView.as_view(url_name='schema'), name='redoc'),
     path('auth/login/', LoginView.as_view(), name='login'),
     path('auth/logout/', LogoutView.as_view(), name='logout'),
     path('auth/registration/', RegisterView.as_view(), name='registration'),
     path('auth/user/', UserDetailsView.as_view(), name='user_details'),
     path('auth/password/reset/', PasswordResetView.as_view(), name='password_reset'),
-    path('auth/password/reset/confirm/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    re_path(
+        r'auth/password/reset/confirm/$',
+        PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm',
+    ),
+    re_path(
+        r'auth/password/reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,32})/$',
+        TemplateView.as_view(template_name='password_reset_confirm.html'),
+        name='password_reset_confirm',
+    ),
     path('auth/password/change/', PasswordChangeView.as_view(), name='password_change'),
     path('auth/', include('dj_rest_auth.urls')),
     path('favicon.ico', RedirectView.as_view(url='/static/favicon.ico')),
